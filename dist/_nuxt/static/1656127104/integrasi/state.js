b46489343c4a05ae61cd026e77955cee4c7654d3
Trisna Chandra Laksana <57003845+Chandralaksana22@users.noreply.github.com>
2022-06-25 10:45:46 +0700

    Fix Dist

---
+++ dist/_nuxt/static/1656127104/integrasi/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1656127104",layout:"default",error:a,state:{about:{list:[]},bannerkj:{list:[]},ekspedisi:{list:[]},mitrakj:{list:[]}},serverRendered:true,routePath:"\u002Fintegrasi",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
